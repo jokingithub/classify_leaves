1560d7a4ba418dc995d03756c76dfeba3e68dcc9
eligah <eligahwsw@163.com>
2024-08-12 12:49:57 +0800

    update ResNet_predict

--- ResNet50_Predict.py
+++ ResNet50_Predict.py
@@ -1,5 +1,3 @@
-import sys
-
 import timm
 from PIL import Image
 import pandas as pd
@@ -8,8 +6,17 @@ import torch.nn as nn
 import json
 import torchvision.transforms as transforms
 
+def is_device():
+    # 检查CUDA是否可用
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+        # print("Running on GPU")
+    else:
+        device = torch.device("cpu")
+        # print("Running on CPU")
+    return device
 
-def preprocess_img(img_path):
+def preprocess_img(img_path, device=is_device()):
     transform_test = transforms.Compose([
         transforms.Resize((112, 112)),
         transforms.ToTensor(),
@@ -22,11 +29,11 @@ def preprocess_img(img_path):
     input_batch = input_tensor.unsqueeze(0)  # 添加一个批次维度
 
     # 如果你的模型在 GPU 上，确保数据也被发送到 GPU
-    input_batch_cuda = input_batch.to('cuda')
+    input_batch_cuda = input_batch.to(device)
     return input_batch_cuda
 
 
-def predict(model_path, img_path, labelmap_path):
+def predict(model_path, img_path, labelmap_path, device=is_device()):
     # 定义模型 加载自定义预训练权重
     model = timm.create_model('resnet50d', pretrained=False)
 
@@ -34,11 +41,10 @@ def predict(model_path, img_path, labelmap_path):
     nums = model.fc.in_features  # 获取最后一层的输入特征数
     model.fc = nn.Linear(nums, 176)  # 更改输出层的大小
 
-    model.load_state_dict(torch.load(model_path, weights_only=False,
-                                      map_location=(torch.device('gpu') if torch.cuda.is_available() else torch.device('cpu'))))
+    model.load_state_dict(torch.load(model_path, weights_only=False))
 
     model.eval()
-    model.cuda()
+    model.to(device)
 
     with torch.no_grad():  # 确保在评估模式下不计算梯度
         x = preprocess_img(img_path)
@@ -54,7 +60,11 @@ def predict(model_path, img_path, labelmap_path):
     value = labelmap.get(pred_i)
     if value is not None:
         prob = "{:.2f}".format(top5_probabilities[0][0])
-        print(f"该叶片预测为：{value}, 准确概率为{prob}%")
+        r = { }
+        r["name"] = value
+        r["class"] = pred_i
+        r["confidence"] = prob
+        return json.dumps(r)
     else:
         print("识别失败，请重新上传图片！")
 
@@ -62,11 +72,10 @@ def predict(model_path, img_path, labelmap_path):
 if __name__ == '__main__':
     model_path = './model/ResNet50_model.pth'
     img_path = './test/58.jpg'
-<<<<<<< Updated upstream
     labelmap_path = './labelmap.json'
-=======
     labelmap_path = './ResNet_labelmap.json'
->>>>>>> Stashed changes
+
+    device = is_device()
 
     res = predict(model_path, img_path, labelmap_path)
-    print(res)
+    print(f"res:{res}")
