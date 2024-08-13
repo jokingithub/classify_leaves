cff7c7bf315438fd49adeecb421312e7d3d6916a
jokin <jokinmail@163.com>
2024-08-13 17:14:53 +0800

    update

--- init_database.py
+++ init_database.py
@@ -1,4 +1,8 @@
 from start import app, DB, LeafCategory
+import csv
+
+
+
 def init_DB():
     with app.app_context():
         DB.create_all()
@@ -9,12 +13,15 @@ def create():
         DB.session.query(LeafCategory).delete()
         DB.session.commit()
 
-        # 添加一些示例数据
-        categories = [
-            LeafCategory(category_name='Oak', chinese_name='xxx', description='A type of deciduous tree.'),
-            LeafCategory(category_name='Maple', chinese_name='xxx', description='A tree known for its colorful leaves.'),
-            # 可以添加更多类别
-        ]
+        categories = []
+        # 打开CSV文件
+        with open('./instance/data.csv', newline='') as csvfile:
+            csvreader = csv.reader(csvfile)
+            
+            # 读取文件的所有行
+            for row in csvreader:
+                # 添加一些示例数据
+                categories.append(LeafCategory(category_name=row[0], chinese_name=row[1], description=row[2]))
         
         DB.session.bulk_save_objects(categories)
         DB.session.commit()
